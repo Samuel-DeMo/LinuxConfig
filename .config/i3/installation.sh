61e5953698ccf8ed9263e252ca7857a53aa2c546
SamDWRay <samuel.dm007@hotmail.com>
2022-12-06 10:17:01 +0100

    Before reinit

--- .config/i3/installation.sh
+++ .config/i3/installation.sh
@@ -1,7 +1,10 @@
 #bin/bash
 
-sudo apt install nvim zsh git flameshot libreoffice
+sudo apt install nvim zsh git flameshot libreoffice vlc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo apt install openjdk-18-jdk mysql-server mysql-client mysql-common
-sudo snap install discord intellij-idea-community
+sudo snap install discord
+sudo snap install intellij-idea-community --classic
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install phpstorm --classic
